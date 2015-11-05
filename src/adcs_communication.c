b46be9f22779e5bc0115c21c865f250b12821ed5
sophia <scastellarin95@gmail.com>
2015-11-04 20:51:17 -0500

    Create interface for adcs communication module

---
+++ src/adcs_communication.c
@@ -0,0 +1,11 @@
+#include <stdlib.h>
+#include "adcs_communication.h"
+
+int get_sensor_data() {
+  return 0;
+}
+
+int send_actuator_data(float x, float y, float z) {
+  return 0;
+}
+
---
+++ src/adcs_communication.h
@@ -0,0 +1,7 @@
+#ifndef ADCS_COMMUNICATION_H_INCLUDED
+#define ADCS_COMMUNICATION_H_INCLUDE
+
+int get_sensor_data();
+int send_actuator_data(float x, float y, float z); 
+ 
+#endif
